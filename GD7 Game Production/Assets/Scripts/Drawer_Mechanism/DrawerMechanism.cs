70ca5f39d018efbafb2b2c8b936175b796f50204
XanDorkiest <xandorkiest@gmail.com>
2024-04-29 15:16:56 +0800

    adasd
    asdasdasd

--- GD7 Game Production/Assets/Scripts/Drawer_Mechanism/DrawerMechanism.cs
+++ GD7 Game Production/Assets/Scripts/Drawer_Mechanism/DrawerMechanism.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DrawerMechanism : MonoBehaviour, IInteractible
 {
-    public GameObject drawer;
     public float speed = 5.0f; // Public speed for movement (adjustable)
     public float offset = 1.0f; // Public offset for movement relative to initial position
     public bool open = false; // Flag to control whether the drawer is open or closed
@@ -38,7 +37,7 @@ public class DrawerMechanism : MonoBehaviour, IInteractible
         inventory = player.GetComponent<PlayerInventory>();
 
         // Store the initial position of the drawer
-        initialPosition = drawer.transform.position;
+        initialPosition = gameObject.transform.position;
 
         // Calculate the target position based on the offset
         targetPosition = initialPosition + transform.right * offset;
@@ -73,29 +72,29 @@ public class DrawerMechanism : MonoBehaviour, IInteractible
     // Move the drawer to the specified target position
     void MoveDrawer(Vector3 targetPosition)
     {
-        Vector3 currentPosition = drawer.transform.position;
+        Vector3 currentPosition = gameObject.transform.position;
 
         // Calculate movement towards the target position
         Vector3 movement = (targetPosition - currentPosition).normalized * speed * Time.deltaTime;
 
         // Update position smoothly
-        drawer.transform.position += movement;
+        gameObject.transform.position += movement;
 
         // Constrain movement within specified limits
         float minX = Mathf.Min(currentPosition.x, targetPosition.x);
         float maxX = Mathf.Max(currentPosition.x, targetPosition.x);
-        currentPosition.x = Mathf.Clamp(drawer.transform.position.x, minX, maxX);
+        currentPosition.x = Mathf.Clamp(gameObject.transform.position.x, minX, maxX);
 
         float minY = Mathf.Min(currentPosition.y, targetPosition.y);
         float maxY = Mathf.Max(currentPosition.y, targetPosition.y);
-        currentPosition.y = Mathf.Clamp(drawer.transform.position.y, minY, maxY);
+        currentPosition.y = Mathf.Clamp(gameObject.transform.position.y, minY, maxY);
 
         float minZ = Mathf.Min(currentPosition.z, targetPosition.z);
         float maxZ = Mathf.Max(currentPosition.z, targetPosition.z);
-        currentPosition.z = Mathf.Clamp(drawer.transform.position.z, minZ, maxZ);
+        currentPosition.z = Mathf.Clamp(gameObject.transform.position.z, minZ, maxZ);
 
         // Update position while considering constraints
-        drawer.transform.position = currentPosition;
+        gameObject.transform.position = currentPosition;
     }
 
     public string GetDisplayText(string key)
