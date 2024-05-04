9305d0d26beade23bcbac7d868ad3d2fe7186a33
Andrei <8700andreihidalgo@gmail.com>
2024-05-05 01:58:56 +0800

    added main menu
    added main menu

---
+++ GD7 Game Production/Assets/Navigation/Gamemode1.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Diagnostics;
+public class Gamemode1 : MonoBehaviour
+{
+    private bool isMenuVisible = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           isMenuVisible = true;
+        }
+
+    }
+       void Start()
+    {
+        Time.timeScale = 1;
+         isMenuVisible = false;
+
+    }
+    public void OnGUI()
+    {
+        if (isMenuVisible)
+        {
+             Cursor.lockState = CursorLockMode.None;
+            Time.timeScale = 0;
+            
+            float menuWidth = 400;  // Define the width of the menu
+            float menuHeight = 200; // Define the height of the menu
+            float menuX = Screen.width / 2 - menuWidth / 2;  // Calculate the x position
+            float menuY = Screen.height / 2 - menuHeight / 2; // Calculate the y position
+            
+            GUI.BeginGroup(new Rect(menuX, menuY, menuWidth, menuHeight));
+            GUI.Box(new Rect(0, 0, menuWidth, menuHeight), "Gamemode 1");
+
+            if (GUI.Button(new Rect(10, 40, menuWidth - 20, 30), "Resume Game"))
+            {
+                ToggleMenu();
+            }
+
+            if (GUI.Button(new Rect(10, 80, menuWidth - 20, 30), "Restart Game"))
+            {
+                RestartGame();
+            }
+
+            if (GUI.Button(new Rect(10, 120, menuWidth - 20, 30), "Back to Main Menu"))
+            {
+                BackToMain();
+            }
+            if (GUI.Button(new Rect(10, 160, menuWidth - 20, 30), "Quit"))
+            {
+                Quit();
+            }
+
+
+
+            GUI.EndGroup();
+        }
+        else{
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void ToggleMenu()
+    {
+        Time.timeScale = 1;
+        isMenuVisible = false;
+         
+    }
+    
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+        void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ GD7 Game Production/Assets/Navigation/Menu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Define textures for button backgrounds
+    public Texture2D startButtonBackground;
+    public Texture2D creditsButtonBackground;
+    public Texture2D quitButtonBackground;
+
+    public void OnGUI()
+    {
+        Cursor.lockState = CursorLockMode.None;
+
+        float menuWidth = 400;  // Define the width of the menu
+        float menuHeight = 200; // Define the height of the menu
+        float menuX = Screen.width / 2 - menuWidth / 2;  // Calculate the x position
+        float menuY = Screen.height - menuHeight; // Calculate the y position
+
+        GUI.BeginGroup(new Rect(menuX, menuY, menuWidth, menuHeight));
+
+        // Define button styles with background images
+        GUIStyle startButtonStyle = new GUIStyle(GUI.skin.button);
+        startButtonStyle.normal.background = startButtonBackground;
+
+        GUIStyle creditsButtonStyle = new GUIStyle(GUI.skin.button);
+        creditsButtonStyle.normal.background = creditsButtonBackground;
+
+        GUIStyle quitButtonStyle = new GUIStyle(GUI.skin.button);
+        quitButtonStyle.normal.background = quitButtonBackground;
+
+        if (GUI.Button(new Rect(20, 20, menuWidth - 60, 50), "", startButtonStyle))
+        {
+            StartGame();
+        }
+        if (GUI.Button(new Rect(20, 80, menuWidth - 60, 50), "", creditsButtonStyle))
+        {
+            StartCredits();
+        }
+        if (GUI.Button(new Rect(20, 140, menuWidth - 60, 50), "", quitButtonStyle))
+        {
+            Quit();
+        }
+
+        GUI.EndGroup();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1 Halfwaydone Joseph");
+    }
+
+    public void StartCredits()
+    {
+        // Load credits scene or perform other actions related to showing credits
+    }
+
+    public void Quit()
+    {
+        UnityEngine.Debug.Log("Quit");
+        Application.Quit();
+    }
+}
